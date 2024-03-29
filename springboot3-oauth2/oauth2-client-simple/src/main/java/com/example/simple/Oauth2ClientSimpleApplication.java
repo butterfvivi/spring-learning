30faca527e8967bba33031325de26600d7b176da
vivi <1975293119@qq.com>
2024-03-29 21:08:56 +0800

    update whole project

---
+++ springboot3-oauth2/oauth2-client-simple/src/main/java/com/example/simple/Oauth2ClientSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ClientSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ClientSimpleApplication.class, args);
+    }
+
+}
---
+++ springboot3-oauth2/springboot3-oauth2-user/src/test/java/org/spring/oauth2/user/SpringOauth2UserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.oauth2.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
