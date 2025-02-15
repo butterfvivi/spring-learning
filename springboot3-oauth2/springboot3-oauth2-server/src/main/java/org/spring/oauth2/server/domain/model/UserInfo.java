a671cb32b22b9f2e6094531bc59ab141f4efd145
vivi <1975293119@qq.com>
2025-02-15 19:22:35 +0800

    update test

---
+++ springboot3-oauth2/springboot3-oauth2-server/src/main/java/org/spring/oauth2/server/domain/model/UserInfo.java
@@ -0,0 +1,48 @@
+package org.spring.oauth2.server.domain.model;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@TableName("sys_user")
+public class UserInfo {
+
+    /**
+     * 主键
+     */
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 名字
+     */
+    private String name;
+
+    /**
+     * 描述
+     */
+    private String description;
+
+    /**
+     * 状态
+     */
+    private Integer status;
+}
---
+++ springboot3-redis/src/test/java/org/vivi/framework/redis/Springboot3RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vivi.framework.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
