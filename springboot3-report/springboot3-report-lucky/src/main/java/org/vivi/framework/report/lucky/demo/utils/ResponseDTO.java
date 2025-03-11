26a617ddf55d56d8adc7b05828bec0c2ba0ca1e4
vivi <1975293119@qq.com>
2025-03-11 22:20:12 +0800

    update Lucky sheet template

---
+++ springboot3-report/springboot3-report-lucky/src/main/java/org/vivi/framework/report/lucky/demo/utils/ResponseDTO.java
@@ -0,0 +1,42 @@
+package org.vivi.framework.report.lucky.demo.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author Mars
+ */
+@Data
+@AllArgsConstructor
+public class ResponseDTO implements Serializable {
+
+    private static final long serialVersionUID = -275582248840137389L;
+
+    private Integer type;
+
+    private String id;
+
+    private String username;
+
+    private String data;
+
+
+    public static ResponseDTO success(String id,String username,String data) {
+        return new ResponseDTO(1,id,username, data);
+    }
+
+    public static ResponseDTO update(String id,String username,String data) {
+        return new ResponseDTO(2,id,username, data);
+    }
+
+    public static ResponseDTO mv(String id,String username,String data) {
+        return new ResponseDTO(3,id,username, data);
+    }
+
+    public static ResponseDTO bulkUpdate(String id,String username,String data) {
+        return new ResponseDTO(4,id,username ,data);
+    }
+
+}
