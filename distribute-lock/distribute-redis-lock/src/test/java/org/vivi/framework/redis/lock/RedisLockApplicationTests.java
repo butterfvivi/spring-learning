5692d14a5a800a58f6f5eafe8b3d46a162452156
vivi <1975293119@qq.com>
2025-07-24 13:16:07 +0800

    redis项目init

---
+++ distribute-lock/distribute-redis-lock/src/test/java/org/vivi/framework/redis/lock/RedisLockApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vivi.framework.redis.lock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
