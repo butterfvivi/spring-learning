47a79458e632beb3a04ffdc750fe8229eca53907
vivi <1975293119@qq.com>
2026-01-17 20:25:14 +0800

    Springboot3集成阿里百炼平台的client

---
+++ springboot3-AI/springboot3-bailian-demo/src/test/java/org/vivi/framework/bailian/demo/BailianDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vivi.framework.bailian.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BailianDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
