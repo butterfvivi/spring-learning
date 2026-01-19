7d164fded532f529de41a570e0021202d25e95e0
vivi <1975293119@qq.com>
2026-01-19 10:07:27 +0800

    Springboot3集成AI，通过prompt实现用户跟AI模型的交互

---
+++ springboot3-AI/springboot3-prompt/src/test/java/org/vivi/framework/prompt/PromptApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vivi.framework.prompt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
